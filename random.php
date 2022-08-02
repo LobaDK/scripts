24af132377234ea632a498ccc51a51a27891df19
LobaDK <gta5.r6s.bf4@gmail.com>
2022-08-02 04:22:10 +0200

    Fat fingered an extra comma

--- random.php
+++ random.php
@@ -1,5 +1,5 @@
 <?php
-$files = glob('./*.{jpg,jpeg,gif,png,webm,mov,mp3,ogg,,mp4}', GLOB_BRACE);
+$files = glob('./*.{jpg,jpeg,gif,png,webm,mov,mp3,ogg,mp4}', GLOB_BRACE);
 $count = count($files);
 $index = rand(0, ($count-1));
 $filename = $files[$index];
