cf3415c4c383d3ce41140967abaf122a12e69337
Nicklas <nicklashelsberg@gmail.com>
2022-09-13 07:48:14 +0200

    added shuffle to list and error detection

--- recursive-desktop-changer.py
+++ recursive-desktop-changer.py
@@ -6,10 +6,16 @@ import time
 
 rd = r'CHANGE_ME_TO_IMAGE_FOLDER_LOCATION'
 
-files = glob.glob('**/*.png',recursive=True, root_dir=rd)
+files = glob.glob('**/*.png',recursive=True, root_dir=rd) #use glob to recursively get all file locations from specified folder
 
-while True:
-    random_image = os.path.join(rd, random.choice(files))
-    print(f'Changing to {random_image}...')
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, random_image, 0)
-    time.sleep(300)
+SPI_SETDESKWALLPAPER = 20 #converted hex code 0x0014 to decimal
+
+random.shuffle(files) #shuffle the list on first run each time, for a little extra randomness
+
+while True: #enter infinite loop
+    random_image = os.path.join(rd, random.choice(files)) #join the root path with a randomly picked file from glob
+    print(f'Changing to {random_image}...\n')
+    wallpaper_changed = ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, random_image, 0) #use SystemParametersInfoW to change desktop wallpaper and save exit code to variable
+    if not bool(wallpaper_changed): #if false, print in the terminal that an error occured
+        print('Error applying wallpaper!\n')
+    time.sleep(300) #wait 5 minutes before applying a new wallpaper
