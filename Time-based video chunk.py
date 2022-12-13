6239c84594b31f41a32be3df933fd599edd3f4aa
LobaDK <lobadk@lobadk.com>
2022-12-13 23:18:46 +0100

    Improved and finalized

--- Time-based video chunk.py
+++ Time-based video chunk.py
@@ -1,10 +1,11 @@
-total_time = 108.400000
-frequency = 4
-last_total_time = round(total_time - int(total_time), 6)
-total_time_without_last = int(total_time - last_total_time)
+total_frames = 6504
+frequency_in_seconds = 10
 
-for i in range(0, int(total_time), frequency):
-    if not i + frequency + last_total_time == total_time:
-        print(f'Cutting from stamp {i} to {(((i + frequency) * 60) - 1) / 60}') # Calculate timestamp to frame, then subtract 1 frame count, and convert back to timestamp
+start_frame = 0
+for i in range(0, int(total_frames / 60), frequency_in_seconds):
+    if not i + frequency_in_seconds >= int(total_frames / 60):
+        end_frame = start_frame + (frequency_in_seconds * 60)
     else:
-        print(f'Cutting from stamp {total_time_without_last} to {total_time}')
+        end_frame = (total_frames - start_frame) + start_frame
+    print(f'Cutting from stamp {start_frame / 60} to {end_frame / 60}')
+    start_frame = end_frame + 1
