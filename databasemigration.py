1e5949b3ee62e2f6d790e419f2c5c7ba9d11ee65
Nicklas H. <gta5.r6s.bf4@gmail.com>
2024-04-16 08:45:08 +0200

    Finished main migration

--- databasemigration.py
+++ databasemigration.py
@@ -1,8 +1,9 @@
 import sqlite3
+from pydantic import BaseModel
 
 
 class oldquantumkatdb:
-    class authenticated_servers:
+    class authenticated_servers(BaseModel):
         id: int
         server_id: int
         server_name: str
@@ -10,7 +11,7 @@ class oldquantumkatdb:
         authenticated_by_name: str
         is_authenticated: int
 
-    class chat:
+    class chat(BaseModel):
         id: int
         user_id: int
         user_name: str
@@ -22,14 +23,14 @@ class oldquantumkatdb:
 
 
 class newquantumkatdb:
-    class authenticated_servers:
+    class authenticated_servers(BaseModel):
         id: int
         server_id: int
         authenticated_by_id: int
         requested_by_id: int
         is_authenticated: int
 
-    class chat:
+    class chat(BaseModel):
         id: int
         user_id: int
         server_id: int
@@ -37,13 +38,13 @@ class newquantumkatdb:
         assistant_message: str
         shared_chat: int
 
-    class users:
+    class users(BaseModel):
         user_id: int
         user_name: str
         agreed_to_tos: int
         is_banned: int
 
-    class servers:
+    class servers(BaseModel):
         server_id: int
         server_name: str
 
@@ -54,8 +55,43 @@ def read_db(db: sqlite3.Connection, table_name: str):
     return cursor.fetchall()
 
 
-old_db = sqlite3.connect("oldquantumkat.db")
-new_db = sqlite3.connect("quantumkat.db")
+old_db = sqlite3.connect("./../oldquantumkat.db")
+new_db = sqlite3.connect("./../quantumkat.db")
 
 old_db_tables = ["authenticated_servers", "chat"]
 new_db_tables = ["authenticated_servers", "chat", "users", "servers"]
+
+for table in old_db_tables:
+    old_data = read_db(old_db, table)
+    new_data = []
+    for row in old_data:
+        if table == "authenticated_servers":
+            new_data.append(
+                newquantumkatdb.authenticated_servers(
+                    id=row[0],
+                    server_id=row[1],
+                    authenticated_by_id=row[3],
+                    requested_by_id=row[3],
+                    is_authenticated=row[5],
+                )
+            )
+        elif table == "chat":
+            new_data.append(
+                newquantumkatdb.chat(
+                    id=row[0],
+                    user_id=row[1],
+                    server_id=row[3],
+                    user_message=row[5],
+                    assistant_message=row[6],
+                    shared_chat=row[7],
+                )
+            )
+
+    cursor = new_db.cursor()
+    cursor.execute(f"DELETE FROM {table}")
+    for row in new_data:
+        cursor.execute(
+            f"INSERT INTO {table} ({','.join(row.dict().keys())}) VALUES ({','.join(['?'] * len(row.dict().keys()))})",
+            list(row.dict().values()),
+        )
+    new_db.commit()
